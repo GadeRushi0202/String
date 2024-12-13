883360f63f6f330b2f5cf064f920e709e7990651
Gade Rushikesh <143445674+GadeRushi0202@users.noreply.github.com>
2024-12-13 15:28:59 +0530

    SendMailWithGoogleSMTP

---
+++ ConsoleApp1/SendMailWithGoogleSMTP.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class SendMailWithGoogleSMTP
+    {
+        static void Main(string[] args)
+        {
+            string fromMail = "gaderushi7393@gmail.com";
+            string fromPassword = "tbid nxpy axgx ophp";
+
+            MailMessage message = new MailMessage();    
+            message.From = new MailAddress(fromMail);
+            message.Subject = "Test Subject";
+            message.To.Add(new MailAddress("gaderushi7393@gmail.com"));
+            message.Body = "<html><body> Test Body </body></html>";
+            message.IsBodyHtml = true;
+
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(fromMail, fromPassword),
+                EnableSsl = true
+            };
+            smtpClient.Send(message);
+
+        }
+    }
+}
